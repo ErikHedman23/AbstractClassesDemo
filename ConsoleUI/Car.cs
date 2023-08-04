0407407ca381832017a16c2881360cf0f4f294eb
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-08-04 12:02:37 -0400

    Added a comment to the Car class

--- ConsoleUI/Car.cs
+++ ConsoleUI/Car.cs
@@ -17,7 +17,7 @@ namespace ConsoleUI
             Console.WriteLine($"I love abstractly driving my {Make} {Model}"); //if you want to inherit the properties of the parent class, you can type this.Model for example to get the original value of the property.
         }
 
-        public virtual void DriveVirtual()
+        public virtual void DriveVirtual() //you actually dont need to put this in this class since you are leaving it as the base value.  It will naturally inherit the base value if you call the method in the main.
         {
             base.DriveVirtual();
         }
